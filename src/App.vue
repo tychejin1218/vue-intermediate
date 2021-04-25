22ed02146fe9a8ba218008a65d48e8deae0ff2eb
tychejin1218 <tychejin@naver.com>
2021-04-25 16:17:44 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+/*var my_cmp = {
+  template: '<div>my compoent</div>'
+}
+
+new Vue({
+  el: '',
+  components: {
+    'my-cmp': mp_cmp
+  }
+});*/
+
+export default {
+  components: {
+    //컴포넌트 태그명 : 컴포넌트 내
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter,
+  }
+}
+</script>
+
+<style>
+
+</style>
