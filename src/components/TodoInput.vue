582abb5e5f7c04d7e32e739a7d64d8da75ea1f61
tychejin1218 <tychejin@naver.com>
2021-04-25 17:27:19 +0900

    1.TodoInput 컴포넌트의 할 일 저장 기능 구현 2.rules 옵션 추가  - "no-console": "off"

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
-	  footer
+	  <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">add</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: function() {
+    return {
+      newTodoItem: ""
+    }
+  },
+  methods: {
+    addTodo: function() {
+      console.log(this.newTodoItem);
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.newTodoItem = '';
+    } 
+  }
 }
 </script>
 
