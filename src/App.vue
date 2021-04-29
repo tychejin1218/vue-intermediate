686cbb21b905e5a9dc6617d1a21543d5b00e2127
tychejin1218 <tychejin@naver.com>
2021-04-29 23:31:40 +0900

    [리펙토링] 할 일 목록 표시 기능

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <TodoHeader></TodoHeader>
     <TodoInput></TodoInput>
-    <TodoList></TodoList>
+    <!-- v-bind:내려보낼 프롭스 속성 이름 = "현재 위치의 컴포넌트 데이터 속성" -->
+    <TodoList v-bind:propsdata="todoItems"></TodoList>
     <TodoFooter></TodoFooter>
   </div>
 </template>
@@ -13,18 +14,21 @@ import TodoInput from './components/TodoInput.vue'
 import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
-/*var my_cmp = {
-  template: '<div>my compoent</div>'
-}
-
-new Vue({
-  el: '',
-  components: {
-    'my-cmp': mp_cmp
-  }
-});*/
-
 export default {
+  data: function() {
+    return {
+      todoItems: []
+    }
+  },
+  created: function() {
+    if(localStorage.length > 0) {
+      for(var i=0; i<localStorage.length; i++) {
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
+      }
+    }
+  },
   components: {
     //컴포넌트 태그명 : 컴포넌트 내
     'TodoHeader': TodoHeader,
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
 	  <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.Item" class="shadow">
+      <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.Item" class="shadow">
         <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
         <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
         <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
@@ -14,11 +14,7 @@
 
 <script>
 export default {
-  data: function() {
-    return {
-      todoItems: []
-    }
-  },
+  props: ['propsdata'],
   methods: {
     removeTodo: function(todoItem, index){
       localStorage.removeItem(todoItem);
@@ -28,15 +24,6 @@ export default {
         todoItem.completed = !todoItem.completed;
         localStorage.removeItem(todoItem.item);
         localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
-    }
-  },
-  created: function() {
-    if(localStorage.length > 0) {
-      for(var i=0; i<localStorage.length; i++) {
-        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-        }
-      }
     }
   }
 }
