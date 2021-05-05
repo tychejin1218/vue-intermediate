08810363c4d8eca466b6230b491475d81ebd9712
tychejin1218 <tychejin@naver.com>
2021-05-05 11:24:27 +0900

    [리펙토링] 속성 메서드의 축약 특징 리팩토링

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -20,14 +20,14 @@
 import Modal from './common/Modal.vue'
 
 export default {
-  data: function() {
+  data() {
     return {
       newTodoItem: "",
       showModal: false
     }
   },
   methods: {
-    addTodo: function() {
+    addTodo() {
       if(this.newTodoItem !== ''){
         //this.$emit('이벤트 이름', 인자1, 인자2, ...);
         this.$emit('addTodoItem', this.newTodoItem);
@@ -36,7 +36,7 @@ export default {
         this.showModal = !this.showModal;
       }
     },
-    clearInput: function() {
+    clearInput() {
       this.newTodoItem = '';
     }
   },
