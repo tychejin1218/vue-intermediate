dfddc0f6ba7759951a0f3e4fa2e2f1f7e58d8149
tychejin1218 <tychejin@naver.com>
2021-05-05 11:05:42 +0900

    화살표 함수

--- src/main.js
+++ src/main.js
@@ -6,3 +6,30 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+
+/*
+화살표 함수 실습
+
+- ES5
+var arr = [1, 2, 3];
+arr.forEach(function(value){
+	console.log(value);
+});
+
+- ES6
+var arr = [1, 2, 3];
+arr.forEach((value) => {
+	console.log(value);
+});
+
+- ES5
+var sum = function() {
+  return 10 + 20;
+};
+
+- ES6
+var sum = () => {
+  return 10 + 20;
+};
+*/
