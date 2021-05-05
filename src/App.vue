d5f667f6a36575a49c1202d7a19b956e02b01b92
tychejin1218 <tychejin@naver.com>
2021-05-05 11:31:11 +0900

    [리펙토링] 속성명의 축약 특징 리팩토링

--- src/App.vue
+++ src/App.vue
@@ -52,10 +52,10 @@ export default {
   },
   components: {
     //컴포넌트 태그명 : 컴포넌트 내
-    'TodoHeader': TodoHeader,
-    'TodoInput': TodoInput,
-    'TodoList': TodoList,
-    'TodoFooter': TodoFooter,
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
   }
 }
 </script>
