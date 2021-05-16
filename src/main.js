0eed77016dbc216910526f3be3c0b84e1d131e04
tychejin1218 <tychejin@naver.com>
2021-05-16 17:59:29 +0900

    state와 getters 소개

--- src/main.js
+++ src/main.js
@@ -113,4 +113,46 @@ Vuex 컨셉
 
 Vuex 구조
   - 컴포넌트 -> 비동기 로직 -> 동기 로직 -> 상태
+
+Vuex 기술 요소
+  - state : 여러 컴포넌트에 공유되는 데이터 data
+  - getters : 연산된 state 값을 접근하는 속성 computed
+  - mutations : state 값을 변경하는 이벤트 로직, 메서드 methods
+  - actions : 비동기 처리 로직을 선언하는 메서드 aysnc methods
+
+state란?
+  - 여러 컴포넌트 간에 공유할 데이터 - 상태
+
+  // Vue
+  data: {
+    message: 'Hello Vue.js!'
+  }
+  // Vuex
+  state: {
+    message: 'Hello Vue.js!'
+  }
+
+  <!-- Vue  -->
+  <p>{{ message }}</p>
+
+  <!-- Vuex-->
+  <p>{{ this.$store.state.message }}</p>
+
+getters란?
+  - state 값을 접근하는 속성이자 computed() 처럼 미리 연산된 값을 접근하는 속성
+  
+  // store.js
+  state: {
+    num: 10
+  },
+  getters: {
+    getNumber(state) {
+      return state.num;
+    },
+    doubleNumber(state) {
+      return state.num * 2;
+  }
+
+  <p>{{ this.$store.getters.getNumber }}</p>
+  <p>{{ this.$store.getters.doubleNumber }}</p>
 */
